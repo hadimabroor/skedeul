c253ab20f1554b3f962a11b171fc5456b7eae0ef
hadi-mabroor <mabroor.hadi@gmail.com>
2024-05-22 01:32:37 +0530

    footer work

--- application/views/landing.php
+++ application/views/landing.php
@@ -74,6 +74,7 @@
                   </li>
                   <li class="group relative">
                     <a
+                      target="_blank"
                       href="<?php echo base_url('pricing') ?>"
                       class="ud-menu-scroll mx-8 flex py-2 text-base font-medium text-dark group-hover:text-primary dark:text-white lg:ml-7 lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 lg:text-white lg:group-hover:text-white lg:group-hover:opacity-70 xl:ml-10"
                     >
