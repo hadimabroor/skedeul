f659444e37698365d62ddc84bd7cd21eee6a55e0
hadi-mabroor <mabroor.hadi@gmail.com>
2024-05-22 15:05:13 +0530

    fixed footer logo

--- application/views/landing.php
+++ application/views/landing.php
@@ -3063,7 +3063,7 @@
                 class="mb-6 inline-block max-w-[160px]"
               >
                 <img
-                  src="assets/images/logo/logo-white.svg"
+                  src="<?= base_url()?>/assets/assets/images/logo/logo-white.svg"
                   alt="logo"
                   class="max-w-full"
                 />
