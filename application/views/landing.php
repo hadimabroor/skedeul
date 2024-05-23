63220cab4f20d4875479bf1461f2b00f1e2cf376
hadi-mabroor <mabroor.hadi@gmail.com>
2024-05-23 09:39:34 +0530

    added login option to mobile nav

--- application/views/landing.php
+++ application/views/landing.php
@@ -108,10 +108,10 @@
                   <?php endif ?>
                   <li class="group relative lg:hidden">
                     <a
-                      href="<?php echo base_url('contact') ?>"
+                      href="<?php echo base_url('login') ?>"
                       class="mx-8 flex py-2 text-base font-medium text-dark group-hover:text-primary dark:text-white lg:ml-7 lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 lg:text-white lg:group-hover:text-white lg:group-hover:opacity-70 xl:ml-10"
                     >
-                      Contact
+                      Log In
                     </a>
                   </li>
                 </ul>
